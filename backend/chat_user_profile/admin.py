9c2a9e5bf22b287822aba4972425ee4f4ecf80fa
Crowdbotics <team@crowdbotics.com>
2020-09-17 23:07:27 +0000

    Update models

---
+++ backend/chat_user_profile/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import VerificationCode, Profile, Contact
+
+admin.site.register(Contact)
+admin.site.register(Profile)
+admin.site.register(VerificationCode)
+
+# Register your models here.
--- backend/your_app_20397/wsgi.py
+++ backend/your_app_20397/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_app_20397.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "your_app_20397.settings")
 
 application = get_wsgi_application()
